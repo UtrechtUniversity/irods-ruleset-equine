4a56363a7c7510a347890a79ed799626c9963224
Ton Smeele <a.p.m.smeele@uu.nl>
2017-10-17 08:35:13 +0200

    merged policies with reserach module

---
+++ uuDgkConstants.r
@@ -0,0 +1,9 @@
+# \file
+# \copyright Copyright (c) 2016, Utrecht university. All rights reserved
+# \license GPLv3, see LICENSE
+
+# \constant UUPRIMARYRESOURCES
+UUPRIMARYRESOURCES = list("irodsResc2");
+
+# \constant UUREPLICATIONRESOURCE
+UUREPLICATIONRESOURCE = "irodsRescRepl";
