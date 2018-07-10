ad6fd7f4dcf617333d42640ff494aee7810509a6
Ton Smeele <a.p.m.smeele@uu.nl>
2018-07-10 19:06:51 +0200

    fix constants file

--- uuDgkConstants.r
+++ uuDgkConstants.r
@@ -12,11 +12,7 @@ UUREPLICATIONRESOURCE = "irodsRescRepl";
 UUMAXREVISIONSIZE = double("2000000000"); # 2GB as in 2 * 1000 * 1000 * 1000
 
 # \constant UUBLACKLIST
-<<<<<<< HEAD
 UUBLACKLIST = list("._*", ".DS_Store");
 
 # \constant UUINSTANCENAME     tijdelijk nodig bij Yoda 1.0-1.2
 UUINSTANCENAME = "dgk";
-=======
-UUBLACKLIST = list("._*", ".DS_Store"); 
->>>>>>> 5973a7fbf04a0eeadb688b4d9270d587f20e8eb8
